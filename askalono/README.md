213db224d9228bbcb05af03acdfc3deaafdcf862
Maximilian Huber <gh@maxhbr.de>
2019-06-09 18:35:37 +0200

    add askalono scanner
    Signed-off-by: Maximilian Huber <gh@maxhbr.de>

---
+++ askalono/README.md
@@ -0,0 +1,12 @@
+# Generated Output
+```
+$ cat _output/time-1.7.tar.gz/askalono/output
+/time-1.7.tar.gz/COPYING
+License: GPL-2.0-only (original text)
+Score: 0.990
+Aliases: GPL-2.0-or-later
+```
+## Expected convertet output
+```
+"COPYING";"GPL-2.0-only,GPL-2.0-or-later";
+```
---
+++ askalono/wrapper.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+# Copyright 2017 Maximilian Huber <oss@maximilian-huber.de>
+# SPDX-License-Identifier: MIT
+set -e
+. "$(dirname "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )")/common.sh"
+
+build() {
+    docker_build_stdin <<'EOF'
+FROM debian:jessie
+
+RUN apt-get update && apt-get install -y wget
+RUN groupadd -r user && useradd --no-log-init -r -m -g user user
+WORKDIR /askalono
+RUN wget https://github.com/amzn/askalono/releases/download/0.3.0/askalono.linux \
+ && chmod +x /askalono/askalono.linux
+
+USER user
+ENTRYPOINT ["/askalono/askalono.linux"]
+CMD ["--help"]
+EOF
+}
+
+run() {
+    docker_rm
+
+    if [[ -d "$1" ]]; then
+        docker_run crawl $1
+    else
+        docker_run $@
+    fi
+}
+
+################################################################################
+if [[ "$1" == "build" ]]; then
+    build
+else
+    run $@
+fi
